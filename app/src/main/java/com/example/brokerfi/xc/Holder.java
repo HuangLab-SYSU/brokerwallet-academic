0c9bbb903d951a20df5d66994edf74d004e65103
blockemulator <huanghw28@mail.sysu.edu.cn>
2025-06-12 00:29:10 +0800

    commit

--- app/src/main/java/com/example/brokerfi/xc/Holder.java
+++ app/src/main/java/com/example/brokerfi/xc/Holder.java
@@ -3,5 +3,5 @@ package com.example.brokerfi.xc;
 public class Holder {
     public static final String serverHost = "academic.broker-chain.com";
     public static final String serverPort = "56741";
-    public static final String contractaddr = "0x61608120CE8F00ECA4BF6EBAF9129Aca0Eeb9561";
+    public static final String contractaddr = "0xF5c7A871DE8fa7A3393C528d57A519DcEB275f19";
 }
